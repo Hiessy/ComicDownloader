51780c4600fea4dc9d8097288210b64b22db7622
martin <diaz.martin06@gmail.com>
2017-02-06 17:58:50 -0300

    modified download start@

--- src/main/java/com/online/download/DownloadStart.java
+++ src/main/java/com/online/download/DownloadStart.java
@@ -18,30 +18,31 @@ public class DownloadStart {
 
 	String globalLink = "http://www.readcomics.tv/comic/";
 	String extension = "jpg";
-	String[] comicNameList = {"criminal-macabre-omnibus-2011",""};
+	String[] comicNameList = { "criminal-macabre-omnibus-2011", "cthulhu-williams-2016", "mythos" };
 
-	for(String comicName : comicNameList){
-	LOGGER.info("Begin download process with arguments: " + globalLink + ", " + extension + ", " + comicName);
+	for (String comicName : comicNameList) {
+	    LOGGER.info("Begin download process with arguments: " + globalLink + ", " + extension + ", " + comicName);
 
-	try {
-	    Map<String, List<String>> downloadLinks = linkGenerator.generateDownloadLinks(globalLink, comicName);
-	    LOGGER.info("Begin downloagin images for all " +downloadLinks.size() +" comics found");
-	    Integer volumeNumber = 1;
-	    Integer pageNumber = 1;
-	    // TODO guardar en un archivo los links de descagaras y el indice
-	    for (; volumeNumber <= downloadLinks.size(); ) {
-		for (String link : downloadLinks.get(volumeNumber.toString())) {
-		    String fileName = comicName + correctNumber(pageNumber++) + "." + extension;
-		    fileDownloader.downloadFile(link, null, null, null, comicName, fileName);
-		    
+	    try {
+		Map<String, List<String>> downloadLinks = linkGenerator.generateDownloadLinks(globalLink, comicName);
+		LOGGER.info("Begin downloagin images for all " + downloadLinks.size() + " comics found");
+		Integer volumeNumber = 1;
+		Integer pageNumber = 1;
+		// TODO guardar en un archivo los links de descagaras y el
+		// indice
+		for (; volumeNumber <= downloadLinks.size();) {
+		    for (String link : downloadLinks.get(volumeNumber.toString())) {
+			String fileName = comicName + correctNumber(pageNumber++) + "." + extension;
+			fileDownloader.downloadFile(link, null, null, null, comicName, fileName);
+
+		    }
+		    volumeNumber++;
 		}
-		volumeNumber++;
+
+	    } catch (Exception e) {
+
+		e.printStackTrace();
 	    }
-
-	} catch (Exception e) {
-
-	    e.printStackTrace();
-	}
 	}
     }
 
