7c97635f78605b1ef962a866d72c3cabf0e5c74b
Martin Diaz <diaz.martin06@gmail.com>
2025-05-16 09:51:48 -0300

    update libs

--- src/main/java/com/online/download/service/LinkGenerator.java
+++ src/main/java/com/online/download/service/LinkGenerator.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 /**
  * @author Tincho
@@ -25,89 +25,89 @@ public class LinkGenerator {
 
     public Map<String, List<String>> generateDownloadLinks(String uri, String comicName) throws Exception {
 
-	log.debug("Generating download link for URL: " + uri + comicName);
-	Map<String, List<String>> downloadLinks = getDownloadURL(uri, comicName);
+        log.debug("Generating download link for URL: " + uri + comicName);
+        Map<String, List<String>> downloadLinks = getDownloadURL(uri, comicName);
 
-	log.debug("finished genetaring map with download links");
+        log.debug("finished genetaring map with download links");
 
-	return downloadLinks;
+        return downloadLinks;
     }
 
     private Map<String, List<String>> getDownloadURL(String url, String comicName) throws IOException {
 
-	Document doc = Jsoup.connect(url + comicName).userAgent("Mozilla").get();
+        Document doc = Jsoup.connect(url + comicName).userAgent("Mozilla").get();
 
-	log.debug("Got document for total amount of comics: " + doc.toString());
+        log.debug("Got document for total amount of comics: " + doc.toString());
 
-	Map<String, List<String>> comicLinks = getComicLinks(doc, comicName);
+        Map<String, List<String>> comicLinks = getComicLinks(doc, comicName);
 
-	log.debug("Total comics found: " + comicLinks.size());
+        log.debug("Total comics found: " + comicLinks.size());
 
-	return comicLinks;
+        return comicLinks;
 
     }
 
     private Map<String, List<String>> getComicLinks(Document doc, String comicName) {
 
-	Map<String, List<String>> comicsList = new HashMap<String, List<String>>();
+        Map<String, List<String>> comicsList = new HashMap<String, List<String>>();
 
-	Elements links = doc.select("a[href]");
-	Integer volumenNumber = 0;
-	log.info("Getting comic book links");
+        Elements links = doc.select("a[href]");
+        Integer volumenNumber = 0;
+        log.info("Getting comic book links");
 
-	for (Element element : links) {
-		log.debug("Analizing link: " + element);
-	    if (element.toString().contains("chapter-") && element.toString().contains(comicName) && !element.toString().contains("single")) {
-		String link = (element.attr("href"));
-		log.info(link);
-		volumenNumber = Integer.valueOf(link.substring(link.lastIndexOf("chapter-") + 8, link.length()));
-		try {
-			log.info("Storing page links for volume number: " + volumenNumber);
-		    if (comicsList.get(volumenNumber.toString()) == null)
-			comicsList.put(volumenNumber.toString(), getPageLinks(Jsoup.connect(link).userAgent("Mozilla").get(), comicName, volumenNumber.toString()));
-		    else
-			log.info("volumen number: " + volumenNumber +", already inserted");
-		} catch (IOException e) {
+        for (Element element : links) {
+            log.debug("Analizing link: " + element);
+            if (element.toString().contains("chapter-") && element.toString().contains(comicName) && !element.toString().contains("single")) {
+                String link = (element.attr("href"));
+                log.info(link);
+                volumenNumber = Integer.valueOf(link.substring(link.lastIndexOf("chapter-") + 8, link.length()));
+                try {
+                    log.info("Storing page links for volume number: " + volumenNumber);
+                    if (comicsList.get(volumenNumber.toString()) == null)
+                        comicsList.put(volumenNumber.toString(), getPageLinks(Jsoup.connect(link).userAgent("Mozilla").get(), comicName, volumenNumber.toString()));
+                    else
+                        log.info("volumen number: " + volumenNumber + ", already inserted");
+                } catch (IOException e) {
 
-		    e.printStackTrace();
-		}
-	    }
+                    e.printStackTrace();
+                }
+            }
 
-	}
+        }
 
-	if (comicsList.size() == 0) {
-	    LOGGER.info("Found no comics in URL, exiting app");
-	    System.exit(0);
-	}
+        if (comicsList.size() == 0) {
+            log.info("Found no comics in URL, exiting app");
+            System.exit(0);
+        }
 
-	LOGGER.info("Finished getting comic book links, sorting..");
+        log.info("Finished getting comic book links, sorting..");
 
-	return comicsList;
+        return comicsList;
     }
 
     private List<String> getPageLinks(Document doc, String comicName, String volumeNumber) {
 
-	List<String> pagesLinks = new ArrayList<String>();
+        List<String> pagesLinks = new ArrayList<String>();
 
-	Elements links = doc.select("div[class=label]");
-	int totalPages = 0;
-	LOGGER.debug("Creating download page link");
+        Elements links = doc.select("div[class=label]");
+        int totalPages = 0;
+        log.debug("Creating download page link");
 
-	for (Element element : links) {
-	    if (element.text().contains("of ")) {
-		totalPages = Integer.valueOf(element.text().replace("of ", ""));
-		LOGGER.debug("The volumen" + volumeNumber + " for the comicbook " + comicName + " has a total of ");
-		break;
-	    }
-	}
+        for (Element element : links) {
+            if (element.text().contains("of ")) {
+                totalPages = Integer.valueOf(element.text().replace("of ", ""));
+                log.debug("The volumen" + volumeNumber + " for the comicbook " + comicName + " has a total of ");
+                break;
+            }
+        }
 
-	for (int i = 0; i < totalPages; i++) {
-	    String page = ("http://www.readcomics.tv/images/manga/" + comicName + "/" + volumeNumber + "/" + (i + 1) + ".jpg");
-	    LOGGER.debug("Creating page link: " + page);
-	    pagesLinks.add(page);
-	}
+        for (int i = 0; i < totalPages; i++) {
+            String page = ("http://www.readcomics.tv/images/manga/" + comicName + "/" + volumeNumber + "/" + (i + 1) + ".jpg");
+            log.debug("Creating page link: " + page);
+            pagesLinks.add(page);
+        }
 
-	return pagesLinks;
+        return pagesLinks;
     }
 
 }
